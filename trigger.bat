45d8a3ff97aea0e0e8e7533566db9042b98579b5
pum-pum <54636165+pum-pum@users.noreply.github.com>
2020-10-28 12:40:45 +0100

    Create trigger.bat

---
+++ trigger.bat
@@ -0,0 +1,36 @@
+@echo off
+color d
+title login
+::doskey twitter=C:\Users\exp\Desktop\scripts\VisualBasicScripts\twitter.vbs
+::doskey alias=doskey /MACROS
+:RETURN
+echo.
+echo.
+echo =============== TWITTER =====================
+echo.
+echo (1) twitter CLI
+
+echo.
+
+set /p caso=elige opcion: 
+
+echo.
+if %caso%==1 goto TWEET
+
+echo.
+echo.
+
+:TWEET
+python C:\Users\exp\scripts\scraping\plantillas\twitter\twitterlineadecomando.py -h
+echo.
+set /p opcion=elige opcion: 
+python C:\Users\exp\scripts\scraping\plantillas\twitter\twitterlineadecomando.py %opcion%
+goto RETURN
+
+goto END
+
+echo.
+goto END
+
+echo.
+:END
